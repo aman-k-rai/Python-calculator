3ffa37f4a0df7a905e7fa905365ed34dd3968310
Aman kumar rai <amanrai276@gmail.com>
2026-03-10 07:43:19 +0000

    Added calculator loop

--- calculator.py
+++ calculator.py
@@ -1,20 +1,23 @@
 print("Python Calculator")
 
-a = float(input("Enter your first number: "))
-op = input("Operator (+,-,*,/): ")
-b = float(input("Enter second number: "))
+while True:
+	a = float(input("Enter your first number: "))
+	op = input("Operator (+,-,*,/): ")
+	b = float(input("Enter second number: "))
 
-if op == "+":
-	print (a + b)
+	if op == "+":
+		print (a + b)
 
-elif op == "-":
-	print (a - b)
+	elif op == "-":
+		print (a - b)
 
-elif op == "*":
-	print (a * b)
+	elif op == "*":
+		print (a * b)
 
-elif op == "/":
-	print (a / b)
+	elif op == "/":
+		print (a / b)
 
-else:
-	print ("invalid operator")
+	again =  input ("Again? (y/n:) ")
+
+	if again != "y":
+		break
